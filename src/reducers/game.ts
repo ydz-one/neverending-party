0434d2c63a5fc5b1ef98757558b57ba41a3e754e
ydz-one <yuduozhao@gmail.com>
2020-01-06 19:51:12 +0300

    Add outcome notification system

--- src/reducers/game.ts
+++ src/reducers/game.ts
@@ -1,3 +1,4 @@
+import { notification } from 'antd';
 import { GameAction, GameActionTypes } from '../actions';
 import { getInitialGameState } from '../utils/data';
 import {
@@ -10,6 +11,11 @@ import {
 export enum GameStatus {
     normal,
     win
+}
+
+enum MessageType {
+    success = 'success',
+    error = 'error'
 }
 
 export interface GameState {
@@ -38,6 +44,46 @@ export interface Npc {
     friends: number[];
 }
 
+const notifyOutcome = (
+    isWin: boolean,
+    student: string,
+    additonalSaved: number
+) => {
+    let messageType: MessageType,
+        message: string,
+        description: string,
+        duration = 2;
+    if (isWin) {
+        messageType = MessageType.success;
+        message = 'Won!';
+        description = additonalSaved
+            ? additonalSaved > 1
+                ? `You won the beer pong duel against ${student}, and also saved ${additonalSaved} other students from the curse!`
+                : `You won the beer pong duel against ${student}, and also saved one other student from the curse!`
+            : `You won the beer pong duel against ${student}!`;
+        duration = additonalSaved ? 4 : 2;
+    } else {
+        messageType = MessageType.error;
+        message = 'Lost!'!;
+        description = `You lost the beer pong duel against ${student}`;
+    }
+    notification[messageType]({
+        message,
+        description,
+        duration,
+        top: 77
+    });
+};
+
+const notifyLevelUp = (level: number) => {
+    notification.success({
+        message: 'Level up!',
+        description: `You are now at level ${level}`,
+        duration: 2,
+        top: 77
+    });
+};
+
 export const gameReducer = (
     state: GameState = getInitialGameState(),
     action: GameAction
@@ -50,6 +96,7 @@ export const gameReducer = (
             const isWin = calcOutcome(winChance);
 
             if (!isWin) {
+                notifyOutcome(false, npc.name, 0);
                 return {
                     ...state,
                     turns: state.turns + 1
@@ -74,8 +121,13 @@ export const gameReducer = (
                 0
             );
 
+            notifyOutcome(true, npc.name, savedNpcs.length - 1);
             const exp = player.exp + expGained;
             const level = calcExpInfo(exp).curLvl;
+
+            if (level > player.level) {
+                notifyLevelUp(level);
+            }
 
             player = {
                 ...player,
